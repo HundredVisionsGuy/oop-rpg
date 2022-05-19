dbddf31d9b1d1298740b7462bed777ed5ab69d35
HundredVisionsGuy <winikkc@hsd.k12.or.us>
2022-05-19 16:34:18 -0700

    adding notes and credits for next time

--- python-oop-rpg/Character.py
+++ python-oop-rpg/Character.py
@@ -3,6 +3,7 @@
 #       allow us to have players AND NPCs that share 
 #       traits
 # 2. Add some stats generation code
+# 3. Add to .gitignore
 
 
 class Player:
@@ -13,3 +14,9 @@ class Player:
 
     def get_stats(self):
         return f"Name: {self.name}\nAge: {self.age}"
+
+
+## Credits:
+# https://www.drivethrurpg.com/product/349171/Micro-Chapbook-RPG-Full-Page-Character-Sheet
+# The 6 Stats: https://tvtropes.org/pmwiki/pmwiki.php/Main/TheSixStats
+# Real Python article: https://realpython.com/python3-object-oriented-programming/#classes-vs-instances
